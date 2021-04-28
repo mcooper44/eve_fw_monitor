35dccd9913fc6a1d954ef3f42149e5d15454bdab
Matt Cooper <m.cooper44@gmail.com>
2021-04-27 23:34:28 -0400

    function that will GET kms from zkill api

---
+++ system_kms.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3.8
+
+import requests
+
+
+USER_AGENT =  ''
+
+def get_zk_response(ID=None, ID_type='system', u_agent=USER_AGENT):
+    '''
+    Form and a Url to GET a json response from zkill of 
+    kill data from an ID_type which could be any one of the fetch
+    modifiers listed in the ID_types dictionary and which are laid out 
+    in the zkill api docs at
+    https://github.com/zKillboard/zKillboard/wiki/API-(Killmails)
+
+    this function returns a data structure with the
+    response
+    ID param fed to the fuction
+    ID_type param fed to the function
+    URL composed by the function and used in the GET request
+    '''
+
+    # these are fetch modifiers.  the applied 
+    # in the following format /ID_type/#/
+    # example https://zkillboard.com/api/kills/regionID/10000002/ 
+    
+    ID_types = {'character' : 'characterID',
+                'corp': 'corporationID',
+                'alliance' : 'allianceID',
+                'faction': 'factionID',
+                'ship' : 'shipTypeID',
+                'group' : 'groupID',
+                'system' : 'solarSystemID',
+                'region' : 'regionID',
+                'war' : 'warID',
+                'value': 'iskValue'}
+    
+    base = 'https://zkillboard.com/api/kills'
+    fetch_mod = ID_types.get(ID_type, False)
+    URL = None
+    
+    if fetch_mod and ID:
+        URL = f'{base}/{fetch_mod}/{ID}/'
+
+    payload = {} 
+    headers = {'User-Agent': USER_AGENT,
+               'Accept-Encoding': 'gzip'}
+
+    response = requests.request("GET", URL, headers=headers, data=payload)
+    
+    return {'response':response, 'ID': ID, 'ID_type': ID_type, 'URL': URL}
+    
+
+if __name__ == '__main__':
+    zk_response = get_zk_response(ID=10000002, ID_type='region')
+    if zk_response['response']:
+        try:
+            print('printing the first few results from the response json')
+            zk_resp = zk_response['response']
+            zk_json = zk_resp.json()
+            print(f'{zk_json[0:10]}')
+        except Exception as error:
+            print('an error was raised!')
+            print(f'by: {zk_response["ID"]} + {zk_response["ID_type"]}')
+            print(error)
+    else:
+        print('failed to get response in main')  
